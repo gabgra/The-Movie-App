b03395200085bbeb0c5be53fa5718ad298225412
Gabriella Gracia <gabriellagraciamt@icloud.com>
2021-01-11 22:12:00 +0700

    Initial Commit

---
+++ The Movie App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Movie App
+//
+//  Created by Gabriella Gracia MT on 11/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
